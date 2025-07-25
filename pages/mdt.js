fd17a7867329979eab3d307112754fc53ff971c1
Icypanic <loganblakey10alt@gmail.com>
2025-07-25 08:17:24 +0100

    fms
    fms

--- pages/mdt.js
+++ pages/mdt.js
@@ -1,12 +1,12 @@
 import Header from '../components/Header';
 
-export default function mdt() {
+export default function MDT() {
   return (
     <div className="bg-primary min-h-screen text-white">
       <Header />
       <main className="p-6">
-        <h1 className="text-2xl font-bold">Training Page</h1>
-        <p>This is where training information will go.</p>
+        <h1 className="text-2xl font-bold">MDT Page</h1>
+        <p>This is the mdt section of the dashboard.</p>
       </main>
     </div>
   );
