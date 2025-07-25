6b0c279a06e6cd39484247510be5dfc5236f4fd7
Icypanic <loganblakey10alt@gmail.com>
2025-07-25 08:04:11 +0100

    Create 999.js

---
+++ pages/999.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+export default function 999() {
+  return (
+    <div className="bg-primary min-h-screen text-white">
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Training Page</h1>
+        <p>This is where training information will go.</p>
+      </main>
+    </div>
+  );
+}
